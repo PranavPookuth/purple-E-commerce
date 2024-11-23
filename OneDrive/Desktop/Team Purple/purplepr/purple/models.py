62fead9baee5fb929e86c986f5dc8e48a4cef958
PranavPookuth <pranavpookuth@gmail.com>
2024-11-23 17:53:39 +0530

    commit

--- OneDrive/Desktop/Team Purple/purplepr/purple/models.py
+++ OneDrive/Desktop/Team Purple/purplepr/purple/models.py
@@ -103,7 +103,6 @@ class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Products, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
-    selected_weight = models.CharField(max_length=50,default="100l")
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
