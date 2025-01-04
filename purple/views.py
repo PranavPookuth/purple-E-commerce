cb7b395e79af00b71afcde4d3873d7fe77322154
PranavPookuth <pranavpookuth@gmail.com>
2025-01-04 16:03:13 +0530

    one drive forlder added

--- purple/views.py
+++ purple/views.py
@@ -126,7 +126,6 @@ class VerifyOTPView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 class RequestOTPView(APIView):
     permission_classes = []
     authentication_classes = []
