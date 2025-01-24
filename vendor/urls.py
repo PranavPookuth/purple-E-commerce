ead4ce4e262692ab463eaf152bd11f64f556a2bb
PranavPookuth <pranavpookuth@gmail.com>
2025-01-24 15:01:04 +0530

    one drive folder added

--- vendor/urls.py
+++ vendor/urls.py
@@ -26,6 +26,8 @@ urlpatterns=[
 #vendor Approval Status
     path('vendor-approval-status/<int:id>/', VendorApprovalStatusView.as_view(), name='vendor-approval-status'),
 
+#vendor Category
+    path('vendors/by-category/', VendorCategoryListView.as_view(), name='vendor-by-category'),
 
 
 ]
