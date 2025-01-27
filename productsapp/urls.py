cb3728d76118fc68841efcf89545dc52edd89f3c
PranavPookuth <pranavpookuth@gmail.com>
2025-01-27 13:00:33 +0530

    one drive folder added

--- productsapp/urls.py
+++ productsapp/urls.py
@@ -5,8 +5,12 @@ urlpatterns = [
 
     path('product/',ProductCreateListView.as_view(),name='product-list-create'),
     path('product/list',ProductCreateListView.as_view(),name='product-list'),
-    path('product/<int:pk>/',ProductCreateDetailView.as_view(),name='product-details-list')
+    path('product/<int:pk>/',ProductCreateDetailView.as_view(),name='product-details-list'),
 
+    path('singleproduct/<int:pk>/',SingleProductView.as_view(),name='single-product'),
+
+    path('vendors/<int:vendor_id>/products',VendorProductListView.as_view(),name='vendor-products-view'),
+    path('vendors/<int:vendor_id>/categories/', VendorCategoryListView.as_view(), name='vendor-category-list'),
 
 
 
